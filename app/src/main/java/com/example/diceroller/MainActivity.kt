8daee0ad35e98e564ecb9a227cf5d6635fbd3a95
Ibrahim Ayub <ibrahimayubadinoyi@gmail.com>
2020-11-09 16:49:36 +0100

    minor code refactoring

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
-import java.security.SecureRandom
 
 class MainActivity : AppCompatActivity() {
     private lateinit var  diceImage: ImageView
@@ -21,7 +20,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-        val imageDrawable = when(SecureRandom().nextInt(6) + 1) {
+        val imageDrawable = when((1..6).shuffled().first()) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
